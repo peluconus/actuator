74b00a0c38593107e334e8a6a8d1edae142664e1
Peluconus <peluconus@hotmail.com>
2021-10-21 08:51:19 +0200

    Commit inicial actuator

---
+++ src/test/java/com/practica/actuator/ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica.actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
